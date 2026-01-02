d653d3d7e7ef33436744033cb010a644d3182284
NaritaShosei <onigirikun7171@gmail.com>
2026-01-03 02:20:20 +0900

    feat:数字に対する画像のデータ

---
+++ Assets/Scripts/SpriteData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ReelData", menuName = "Data")]
+public class ReelData : ScriptableObject
+{
+    public SpriteData[] Data => _data;
+    [SerializeField] private SpriteData[] _data;
+}
+
+[Serializable]
+public struct SpriteData
+{
+    public int Symbol => _symbol;
+    public Sprite Sprite => _sprite;
+
+    [SerializeField] private int _symbol;
+    [SerializeField] private Sprite _sprite;
+}
