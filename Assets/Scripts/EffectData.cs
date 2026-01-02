1b610ead3c9e4a1b07644e0398dd78d8ccc388f9
NaritaShosei <onigirikun7171@gmail.com>
2026-01-03 05:40:04 +0900

    feat:結果に応じて再生する演出を変更できるようなクラス

---
+++ Assets/Scripts/EffectData.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "EffectData", menuName = "Data")]
+
+public class EffectData : ScriptableObject
+{
+    public int Symbol => _symbol;
+    public SlotResultType ResultType => _resultType;
+    public GameObject EffectPrefab => _effectPrefab;
+
+    [SerializeField] private int _symbol;
+    [SerializeField] private SlotResultType _resultType;
+    [SerializeField] private GameObject _effectPrefab;
+}
---
+++ Assets/Scripts/ResultEffectController.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using UnityEngine;
+
+public class ResultEffectController : MonoBehaviour
+{
+    public void PlayEffect(int[] reel)
+    {
+        var result = ResultJudge.Judge(reel);
+
+        Show(result);
+    }
+
+    [SerializeField] private EffectData[] _effects;
+
+    private void Show(SlotResult result)
+    {
+        var effect = _effects.FirstOrDefault(e =>
+            e.Symbol == result.Symbol &&
+            e.ResultType == result.Type);
+
+        if (effect == null) return;
+
+        Instantiate(effect.EffectPrefab);
+    }
+}
