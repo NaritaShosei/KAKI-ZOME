901db9a66b2eac60e33be298c1523e1fcf845e3d
NaritaShosei <onigirikun7171@gmail.com>
2026-01-02 22:45:48 +0900

    feat:スロット部分

---
+++ Assets/Scripts/Slot.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class Slot
+{
+    public Slot(float changeTime)
+    {
+        _changeTime = changeTime;
+    }
+    public int GetSymbol()
+    {
+        return _reel[_currentIndex];
+    }
+
+    public void rotateReel(float deltaTime)
+    {
+        _timer += deltaTime;
+
+        if (_changeTime <= _timer)
+        {
+            _changeTime = 0;
+            _currentIndex = ++_currentIndex % _reel.Length;
+        }
+    }
+
+    [SerializeField] private int[] _reel;
+    private float _timer;
+    private float _changeTime;
+    private int _currentIndex;
+}
