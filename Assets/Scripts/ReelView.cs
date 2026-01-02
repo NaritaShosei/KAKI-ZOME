79553c83c0e94c0a88594d1ad5cdd2f304e6720c
NaritaShosei <onigirikun7171@gmail.com>
2026-01-03 02:19:08 +0900

    feat:スロットの見た目

---
+++ Assets/Scripts/ReelView.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ReelView : MonoBehaviour
+{
+    public void Init(Slot slot)
+    {
+        _slot = slot;
+    }
+
+    public void UpdateView()
+    {
+        var reel = _slot.GetCurrentReel();
+
+        Set(_twoUp, reel.twoUp);
+        Set(_up, reel.up);
+        Set(_now, reel.now);
+        Set(_down, reel.down);
+        Set(_twoDown, reel.twoDpwn);
+        Set(_twoDownUp, reel.twoDpwn);
+
+        float y = -reel.offset * _symbolHeight;
+
+        _twoDownUp.anchoredPosition = new Vector2(_now.anchoredPosition.x, _base.anchoredPosition.y + y - _symbolHeight * 3);
+        _twoUp.anchoredPosition = new Vector2(_now.anchoredPosition.x, _base.anchoredPosition.y + y - _symbolHeight * 2);
+        _up.anchoredPosition = new Vector2(_now.anchoredPosition.x, _base.anchoredPosition.y + y - _symbolHeight);
+        _now.anchoredPosition = new Vector2(_now.anchoredPosition.x, _base.anchoredPosition.y + y);
+        _down.anchoredPosition = new Vector2(_now.anchoredPosition.x, _base.anchoredPosition.y + y + _symbolHeight);
+        _twoDown.anchoredPosition = new Vector2(_now.anchoredPosition.x, _base.anchoredPosition.y + y + _symbolHeight * 2);
+    }
+
+    public void StopView()
+    {
+        var reel = _slot.GetCurrentReel();
+
+        Set(_twoUp, reel.twoUp);
+        Set(_up, reel.up);
+        Set(_now, reel.now);
+        Set(_down, reel.down);
+        Set(_twoDown, reel.twoDpwn);
+        Set(_twoDownUp, reel.twoDpwn);
+
+        float y = _base.anchoredPosition.y;
+
+        _twoDownUp.anchoredPosition = new Vector2(_now.anchoredPosition.x, _base.anchoredPosition.y + y - _symbolHeight * 3);
+        _twoUp.anchoredPosition = new Vector2(_now.anchoredPosition.x, _base.anchoredPosition.y + y - _symbolHeight * 2);
+        _up.anchoredPosition = new Vector2(_now.anchoredPosition.x, _base.anchoredPosition.y + y - _symbolHeight);
+        _now.anchoredPosition = new Vector2(_now.anchoredPosition.x, _base.anchoredPosition.y + y);
+        _down.anchoredPosition = new Vector2(_now.anchoredPosition.x, _base.anchoredPosition.y + y + _symbolHeight);
+        _twoDown.anchoredPosition = new Vector2(_now.anchoredPosition.x, _base.anchoredPosition.y + y + _symbolHeight * 2);
+    }
+
+    private Slot _slot;
+    [SerializeField] private RectTransform _twoUp;
+    [SerializeField] private RectTransform _up;
+    [SerializeField] private RectTransform _now;
+    [SerializeField] private RectTransform _down;
+    [SerializeField] private RectTransform _twoDown;
+    [SerializeField] private RectTransform _twoDownUp; // 下の画像が見切れるタイミングで一番上にくる
+
+    [SerializeField] private RectTransform _base;
+    [SerializeField] private float _symbolHeight;
+    [SerializeField] private ReelData _data;
+
+    private void Set(RectTransform rt, int symbol)
+    {
+        // symbol → Sprite 変換
+        rt.GetComponent<Image>().sprite = GetSprite(symbol);
+    }
+
+    private Sprite GetSprite(int symbol)
+    {
+        return _data.Data.Where(data => data.Symbol == symbol).FirstOrDefault().Sprite;
+    }
+}
